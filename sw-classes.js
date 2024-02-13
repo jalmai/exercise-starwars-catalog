9fb437f727bb017866af09e5ecd0ce9dccf95684
mingman <ingman.m@gmail.com>
2024-02-13 09:01:19 +0100

    separate into smaller files

--- sw-classes.js
+++ sw-classes.js
@@ -1,12 +1,6 @@
-let chars = [];
-let charList = document.querySelector(".char-list");
-let homeWorldsUrl = ["unknown"];
-let planets = [];
-let currentActive = 0;
 class StarWarsCharacter {
   // TODO: Do not set data as URL, instead set as null until data is gathered
   //TODO: Separate into different functions in construct
-  // TODO: Separate into smaller files
   constructor(data) {
     this.name = data.name || "Unknown";
     this.birthYear = data.birth_year || "Unknown";
@@ -117,99 +111,4 @@ class Planet {
     });
     return ul;
   }
-}
-
-charList.classList.add("loader");
-makeRequest("GET", "https://swapi.dev/api/people/", function (error, data) {
-  getAllChars(error, data);
-});
-
-function getAllChars(error, data) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log(data.next);
-    // !Temporary commented out to avoid unecessary calls from api
-    // if (data.next) {
-    //   makeRequest("GET", data.next, function (error, data) {
-    //     getAllChars(error, data);
-    //   });
-    // } else {
-    //   console.log("gathered all character data");
-    //   console.log(chars);
-    // }
-
-    data.results.forEach((element) => {
-      let char = new StarWarsCharacter(element);
-      chars.push(char);
-    });
-    let htmlList = document.createElement("ul");
-    chars.forEach((char, i) => {
-      let li = document.createElement("li");
-      li.addEventListener("click", function () {
-        activeTab(i);
-        charDetails(currentActive);
-      });
-      li.innerText = char.name;
-      htmlList.appendChild(li);
-    });
-    charList.innerHTML = "";
-    charList.classList.remove("loader");
-    charList.appendChild(htmlList);
-    let button = document.createElement("button");
-    button.innerHTML = "next character";
-    button.addEventListener("click", function () {
-      if (currentActive != null) {
-        if (currentActive === charList.getElementsByTagName("li").length - 1) {
-          currentActive = 0;
-        } else {
-          currentActive += 1;
-        }
-      } else {
-        currentActive = 0;
-      }
-      charDetails(currentActive);
-      activeTab(currentActive);
-    });
-    charList.appendChild(button);
-    activeTab(currentActive);
-  }
-}
-
-function makeRequest(method, url, callback) {
-  const xhr = new XMLHttpRequest();
-  xhr.open(method, url);
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === XMLHttpRequest.DONE) {
-      if (xhr.status >= 200 && xhr.status < 300) {
-        const responseData = JSON.parse(xhr.responseText);
-        callback(null, responseData);
-      } else {
-        callback(new Error(`Request failed with status ${xhr.status}`), null);
-      }
-    }
-  };
-  xhr.onerror = function () {
-    callback(new Error("Request failed"), null);
-  };
-  xhr.send();
-}
-function charDetails(index) {
-  let info = chars[index].getInfoAsList();
-  let currPlanet = chars[index].homeworld;
-  let planInfo = planets[currPlanet - 1].getInfoAsList();
-  let charDetails = document.querySelector(".char-detail");
-  charDetails.innerHTML = "";
-  charDetails.appendChild(info);
-  let planetDetails = document.querySelector(".planet-detail");
-  planetDetails.innerHTML = "";
-  planetDetails.appendChild(planInfo);
-}
-function activeTab(i) {
-  let li = charList.getElementsByTagName("li");
-  Array.from(li).forEach((element) => {
-    element.classList.remove("active");
-  });
-  li[i].classList.add("active");
-  currentActive = i;
 }
